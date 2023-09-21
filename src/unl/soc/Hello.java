47f499f38e903127b0b3ff2392603a6a16f9d5d9
nwilber9 <142922865+nwilber9@users.noreply.github.com>
2023-09-21 16:01:29 +0000

    Added Name and Major

--- src/unl/soc/Hello.java
+++ src/unl/soc/Hello.java
@@ -2,5 +2,8 @@ class Hello {
     public static void main(String[] args){
         System.out.println("Noah Wilber");
         System.out.println("Computer Engineering");
+
+        System.out.println("Boah Bilbur")
+        System.out.println("Engineering Computers")
     }
 }
